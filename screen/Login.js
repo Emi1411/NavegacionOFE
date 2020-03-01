548d37a44239d242d7c50683e139a375e04e5597
Emi1411 <olverafabianemi@gmail.com>
2020-02-29 22:09:48 -0600

    Modificacion

--- screen/Login.js
+++ screen/Login.js
@@ -1,21 +1,54 @@
 import React,{Component} from 'react';
-import { StyleSheet} from 'react-native';
+import {StyleSheet, ActivityIndicator,Modal,View} from 'react-native';
 import { Container, Header, Content, Card, CardItem, Text, Body,Button,Item,Input,Icon } from 'native-base';
 
 class Login extends Component {
+
   constructor(props){
     super(props);
     this.state={
       nombre:'',
-      password:''
+      password:'',
+      isloading: true,
     }
   }
+  ShowHideActivityIndicator = () =>{
+    const navegar=this.props.navigation;
+    if(this.state.isLoading == true)
+    {
+      this.setState({isLoading: false})
+    }
+    else
+    {
+      this.setState({isLoading: true})
+      setTimeout(() => {
+        navegar.navigate('Usuario',{nombres:this.state.nombre,pass:this.state.password});
+        this.setState({isLoading: false})
+      },2000);
+    }
+  }
+  
 
 render(){
   const navegar=this.props.navigation;
   return (
     <Container>
         <Header />
+        <View style={misEstilos.MainContainer}>
+        {
+        this.state.isLoading ?  
+        <Modal
+        transparent = {true} 
+        animationType = {'none'} 
+        visible = {this.state.isloading}> 
+          <View style = {misEstilos.modalBackground}> 
+            <View style = {misEstilos.activityIndicatorWrapper}> 
+              <ActivityIndicator style={{padding: 70}} /> 
+            </View> 
+          </View> 
+        </Modal> : null
+        }
+        </View>
         <Content padder contentContainerStyle ={misEstilos.content}>
           <Card>
             <CardItem header bordered>
@@ -34,7 +67,7 @@ render(){
               </Body>
             </CardItem>
             <CardItem footer bordered>
-            <Button primary style={misEstilos.boton} onPress={() => {navegar.navigate('Usuario',{nombres:this.state.nombre,pass:this.state.password});}}><Text> Entrar </Text></Button>
+            <Button primary style={misEstilos.boton} onPress={this.ShowHideActivityIndicator}><Text> Entrar </Text></Button>
             </CardItem>
           </Card>
           <Text style={misEstilos.textCenter}>No tienes cuenta:</Text>
@@ -61,6 +94,26 @@ const misEstilos = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  MainContainer :{
+    justifyContent: 'center',
+  },
+  modalBackground : {
+    flex : 1 ,
+    alignItems : 'center' ,
+    flexDirection : 'column' ,
+    justifyContent : 'space-around' ,
+    backgroundColor : '#00000040'
+  } ,
+  activityIndicatorWrapper : {
+    backgroundColor : '#FFFFFF' ,
+    height : 100 ,
+    width : 100 ,
+    borderRadius : 10 ,
+    display : 'flex' ,
+    alignItems : 'center' ,
+    justifyContent : 'space-around'
+  }
 });
 
+//AppRegistry.registerComponent('Login', () => Login);
 export default Login;
